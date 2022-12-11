e03dcbfb098b7581ed02b76d114e28336a9fe12a
hheeseung <ssigu69@gmail.com>
2022-12-11 21:22:41 +0900

    dir: 폴더 구조 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Switch } from "react-router-dom";
-import { Login } from "./routes/Login";
-import { PrivateRoute } from "./routes/redirect/PrivateRoute";
-import { PublicRoute } from "./routes/redirect/PublicRoute";
-import { Signup } from "./routes/Signup";
+import { Login } from "./components/Auth/Login";
+import { PrivateRoute } from "./routes/Redirect/PrivateRoute";
+import { PublicRoute } from "./routes/Redirect/PublicRoute";
+import { Signup } from "./components/Auth/Signup";
 import { Todo } from "./routes/Todo";
 
 export function Router() {
